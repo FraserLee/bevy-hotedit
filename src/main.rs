ee7dd66ac1724f39ba7f6e2c6c1ae8c2a02c67ed
Fraser <tomatoesneverlie@gmail.com>
2022-06-16 19:26:17 -0400

    added bevy dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+#[test]
+fn startup_test() {
+    App::new()
+        .add_startup_system( | | {
+            assert_eq!(0, 1);
+        })
+        .run();
 }
+
