81c37b392eb2177ba5c4c49c4c5e24caef9965ce
Fraser <tomatoesneverlie@gmail.com>
2022-06-16 20:37:10 -0400

    skeleton macro attribute

--- bevy_hotedit/src/lib.rs
+++ bevy_hotedit/src/lib.rs
@@ -1,3 +1,19 @@
-pub fn one() -> i32 { 1 }
+use proc_macro::TokenStream;
+use quote::quote;
+// use syn;
+
+#[proc_macro_attribute]
+pub fn set_const_value(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // replace whatever's below the macro to 'const #name: i32 = 1;'
+    println!("item: \"{}\"", item.to_string());
+
+    let new_item: TokenStream = quote! {
+        const X: i32 = 1;
+    }.into();
+
+    println!("new_item: \"{}\"", new_item.to_string());
+
+    new_item
+}
 
 
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -4,20 +4,18 @@ use bevy_hotedit::*;
 #[test]
 fn test_pass() {
     App::new()
-        .add_startup_system( | | {
-            assert_eq!(one(), 1);
-        })
+        .add_startup_system( | | { assert_eq!(X, 1); })
         .run();
 }
 
 #[test]
 fn test_fail() {
     App::new()
-        .add_startup_system( | | {
-            assert_eq!(one(), 2);
-        })
+        .add_startup_system( | | { assert_eq!(X, 2); })
         .run();
 }
 
+#[set_const_value]
+const X:i32;
 
 
